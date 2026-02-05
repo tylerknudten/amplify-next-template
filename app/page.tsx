f55f3070df316fd44a55c34612cd04551e2e2b69
RipRed1 <tsnipes164@gmail.com>
2026-02-05 12:18:15 -0800

    check

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+
 import { useState, useEffect } from "react";
+import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
